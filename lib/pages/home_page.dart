50d4364d38ce0ad62a45c463603138b3e30f8794
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-12 20:09:57 -0300

    Adicionada correções

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gs/models/eletropostos_model.dart';
 import 'package:provider/provider.dart';
 import 'package:gs/providers/etropostos_provider.dart';
 
@@ -12,7 +11,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    // Chama o método fetchEletropostos quando a página for carregada
     Future.delayed(Duration.zero, () {
       Provider.of<EletropostoProvider>(context, listen: false)
           .fetchEletropostos();
@@ -23,19 +21,16 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Eletropostos'),
+        title: const Text('Eletropostos'),
       ),
       body: Consumer<EletropostoProvider>(
         builder: (context, provider, child) {
           if (provider.isLoading) {
-            // Exibe o CircularProgressIndicator enquanto está carregando
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (provider.errorMessage.isNotEmpty) {
-            // Exibe uma mensagem de erro, se houver
             return Center(child: Text('Erro: ${provider.errorMessage}'));
           } else if (provider.eletropostos.isEmpty) {
-            // Exibe uma mensagem caso não haja eletropostos
-            return Center(child: Text('Nenhum eletroposto encontrado.'));
+            return const Center(child: Text('Nenhum eletroposto encontrado.'));
           } else {
             final eletropostos = provider.eletropostos;
             return ListView.builder(
@@ -43,28 +38,27 @@ class _HomePageState extends State<HomePage> {
               itemBuilder: (context, index) {
                 final eletroposto = eletropostos[index];
                 return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                   child: ListTile(
                     title: Text(
                       eletroposto.nome,
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
                     ),
                     subtitle: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // Informações adicionais
                         Text(eletroposto.informacoes),
-                        SizedBox(height: 4),
+                        const SizedBox(height: 4),
                         Text('Endereço: ${eletroposto.endereco}'),
-                        SizedBox(height: 4),
+                        const SizedBox(height: 4),
                         Text('Telefone: ${eletroposto.telefone}'),
-                        SizedBox(height: 4),
+                        const SizedBox(height: 4),
                         Text('Conectores: ${eletroposto.conectores.join(", ")}'),
                       ],
                     ),
-                    trailing: Icon(Icons.ev_station),
+                    trailing: const Icon(Icons.ev_station),
                     onTap: () {
-                      // Ação ao clicar no eletroposto (poderia ser para abrir mais detalhes)
+
                     },
                   ),
                 );
